c04d4f2e20fcb012acc3b0c9765747cf9d0230fa
Roger Rajaratnam <roger@sourcier.uk>
2021-02-26 11:50:06 +0000

    👷 Configure Cognito Identity Pool in CDK

--- infrastructure/lib/index.js
+++ infrastructure/lib/index.js
@@ -6,7 +6,7 @@ import DynamoDBStack from "./DynamoDBStack";
 export default function main(app) {
   new DynamoDBStack(app, "dynamodb");
 
-  new S3Stack(app, "s3");
+  const s3 = new S3Stack(app, "s3");
 
-  new CognitoStack(app, "cognito");
+  new CognitoStack(app, "cognito", { bucketArn: s3.bucket.bucketArn });
 }
