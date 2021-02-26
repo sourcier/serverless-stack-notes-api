2bdc1734a2a08fe87180f131323843d0d67d04a6
Roger Rajaratnam <roger@sourcier.uk>
2021-02-26 11:40:35 +0000

    👷 Configure S3 in CDK

--- infrastructure/lib/index.js
+++ infrastructure/lib/index.js
@@ -1,6 +1,9 @@
+import S3Stack from "./S3Stack";
 import DynamoDBStack from "./DynamoDBStack";
 
 // Add stacks
 export default function main(app) {
   new DynamoDBStack(app, "dynamodb");
+
+  new S3Stack(app, "s3");
 }
